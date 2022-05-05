d40fa63733082cb05fe6b4f3c238886c3e8ec48e
CGfrac <CGfrac@protonmail.com>
2022-05-05 10:58:27 +0200

    Refactor

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-const container = document.querySelector('#container');
-
-// We remove 2px because of the borders
-const CONTAINER_SIDE_LENGTH = container.offsetWidth - 2;
-
 function createGrid(squaresPerSide) {
+    const container = document.querySelector('#container');
+    // We remove 2px because of the borders
+    const containerSideLength = container.offsetWidth - 2;
+
     const numberOfElements = squaresPerSide * squaresPerSide;
+    const sideLength = containerSideLength / squaresPerSide;
 
     for (let i = 0; i < numberOfElements; i++) {
         const pixel = document.createElement('div');
         pixel.className = 'pixel';
-
-        const sideLength = CONTAINER_SIDE_LENGTH / squaresPerSide;
 
         pixel.style.height = `${sideLength}px`;
         pixel.style.width = `${sideLength}px`;
