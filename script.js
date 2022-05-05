6e33169dfdaf10c81bf5f39dd5b660f463940c81
CGfrac <CGfrac@protonmail.com>
2022-05-05 12:06:25 +0200

    Implement way to change grid size

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const MIN_SQUARES_PER_SIDE = 1;
+const MAX_SQUARES_PER_SIDE = 100;
+const container = document.querySelector('#container');
+
+function setPixelsListeners() {
+    pixels = document.querySelectorAll('.pixel');
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('mouseover', changePixelColor);
+    });
+}
+
 function createGrid(squaresPerSide) {
-    const container = document.querySelector('#container');
-    // We remove 2px because of the borders
+    // Remove 2px because of the borders
     const containerSideLength = container.offsetWidth - 2;
 
     const numberOfElements = squaresPerSide * squaresPerSide;
@@ -15,15 +25,47 @@ function createGrid(squaresPerSide) {
 
         container.appendChild(pixel);
     }
+
+    setPixelsListeners();
 }
 
 function changePixelColor(event) {
     event.target.style.backgroundColor = 'black';
 }
 
+function promptUser() {
+    let value;
+    do {
+        const input = prompt(`How many squares per side (between ${MIN_SQUARES_PER_SIDE} and ${MAX_SQUARES_PER_SIDE})?`);
+
+        // Make sure user can cancel the prompt
+        if (input === null) {
+            return input;
+        }
+
+        value = parseInt(input, 10);
+    } while (isNaN(value) ||
+        value < MIN_SQUARES_PER_SIDE ||
+        value > MAX_SQUARES_PER_SIDE);
+    
+    return value;
+}
+
+function clearGrid() {
+    while (container.lastChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+function changeGrid() {
+    const input = promptUser();
+    if (input === null) return;
+
+    clearGrid();
+    createGrid(input);
+}
+
 createGrid(16);
 
-pixels = document.querySelectorAll('.pixel');
-pixels.forEach((pixel) => {
-    pixel.addEventListener('mouseover', changePixelColor);
-});
+changeButton = document.querySelector('#set-grid-size');
+changeButton.addEventListener('click', changeGrid);
